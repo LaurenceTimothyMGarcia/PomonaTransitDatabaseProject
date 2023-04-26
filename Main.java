f27f8830a7af1a76725cbf8a89e8281ccd1dc78e
Laurence Timothy Garcia <45772910+LaurenceTimothyMGarcia@users.noreply.github.com>
2023-04-25 19:47:28 -0700

    added comments of instructions

---
+++ Main.java
@@ -0,0 +1,21 @@
+// Laurence Garcia
+// Sunjay
+// CS 4350
+// Lab 4
+
+// Project Details
+    // Design and implement a database system of the Pomona Transit System with Database of choice and JDBC
+    // 1. Display the schedule of all trips for a given StartLocationName and Destination Name, and Date. 
+        // In addition to these attributes, the schedule includes: Scheduled StartTime, ScheduledArrivalTime , DriverID, and BusID.
+    // 2. Edit the schedule i.e. edit the table of Trip Offering as follows:
+        // -Delete a trip offering specified by Trip#, Date, and ScheduledStartTime;
+        // -Add a set of trip offerings assuming the values of all attributes are given (the software asks if you have more trips to enter) ;
+        // - Change the driver for a given Trip offering (i.e given TripNumber, Date, ScheduledStartTime);
+        // - Change the bus for a given Trip offering
+    // 3. Display the stops of a given trip ( i.e. the attributes of the table TripStopInfo).
+    // 4. Display the weekly schedule of a given driver and date.
+    // 5. Add a drive.
+    // 6. Add a bus.
+    // 7. Delete a bus.
+    // 8. Record (insert) the actual data of a given trip offering specified by its key. The actual
+    // data include the attributes of the table ActualTripStopInfo.
